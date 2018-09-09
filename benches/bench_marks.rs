133ea8684653de4860c9e56807c8c825facab534
Miles Granger <miles59923@gmail.com>
2018-09-09 16:16:25 +0200

    Simplify bench testing setup

--- benches/bench_marks.rs
+++ benches/bench_marks.rs
@@ -6,31 +6,37 @@ use blackjack::prelude::*;
 use criterion::Criterion;
 
 
-fn bench_series_sum() {
-    let series = Series::arange(0, 10000);
-    series.sum::<i64>();
-}
-
-fn bench_series_min() {
-    let series = Series::arange(0, 10000);
-    series.min::<i64>().unwrap();
-}
-
-fn bench_series_max() {
-    let series = Series::arange(0, 10000);
-    series.max::<i64>().unwrap();
-}
-
-fn bench_series_mean() {
-    let series = Series::arange(0, 10000);
-    series.mean().unwrap();
-}
-
 fn criterion_bechmark(c: &mut Criterion) {
-    c.bench_function("series min",  |b| b.iter(|| bench_series_min()));
-    c.bench_function("series max",  |b| b.iter(|| bench_series_max()));
-    c.bench_function("series mean", |b| b.iter(|| bench_series_mean()));
-    c.bench_function("series sum",  |b| b.iter(|| bench_series_sum()));
+
+    let inputs = vec![
+        Series::arange(0, 100000)
+    ];
+
+    c.bench_function_over_inputs(
+        "series min",  
+        |b, series| b.iter(|| series.min::<i64>()),
+        inputs.clone()
+    );
+
+    c.bench_function_over_inputs(
+        "series max",  
+        |b, series| b.iter(|| series.max::<i64>()),
+        inputs.clone()
+    );
+
+    c.bench_function_over_inputs(
+        "series mean",  
+        |b, series| b.iter(|| series.mean()),
+        inputs.clone()
+    );
+    
+    c.bench_function_over_inputs(
+        "series sum",  
+        |b, series| b.iter(|| series.sum::<i64>()),
+        inputs.clone()
+    );
+    
+
 }
 
 criterion_group!(benches, criterion_bechmark);
