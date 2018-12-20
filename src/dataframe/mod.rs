be5333c88312b868b242f30967241e2b6a1fad86
Miles Granger <miles59923@gmail.com>
2018-12-20 05:47:34 +0100

    Convert to Reader/Writer for DataFrame io

--- src/dataframe/mod.rs
+++ src/dataframe/mod.rs
@@ -2,14 +2,14 @@
 //!
 //!
 
-use std::path::Path;
-use std::ffi::OsStr;
-
 use bincode::{serialize_into, deserialize_from};
 use csv;
 use serde::{Deserialize};
 
 use prelude::*;
+
+pub mod io;
+pub use self::io::*;
 
 
 /// Common error enum for the crate
@@ -231,154 +231,5 @@ impl DataFrame {
     /// Get the number of columns for this dataframe
     pub fn n_columns(&self) -> usize {
         self.data.len()
-    }
-
-    /// Read a CSV file into a [`DataFrame`] where each column represents a Series
-    /// supports automatic decompression of gzipped files if they end with `.gz`
-    ///
-    /// ## Example
-    ///
-    /// ```
-    /// use blackjack::prelude::*;
-    ///
-    /// let path = format!("{}/tests/data/basic_csv.csv", env!("CARGO_MANIFEST_DIR"));
-    /// let df = DataFrame::read_csv(&path, b',').unwrap();
-    ///
-    /// let col1: Series<f32> = df.get_column("col1").unwrap();
-    /// assert_eq!(col1.sum() as i32, 15);
-    ///
-    /// ```
-    pub fn read_csv<S>(path: S, delimiter: u8) -> Result<Self, BlackJackError>
-        where S: AsRef<OsStr> + ToString
-    {
-
-        use std::io::prelude::*;
-        use std::fs::File;
-        use flate2::read::GzDecoder;
-
-        let p = Path::new(&path);
-        let file_reader: Box<Read> = if path.to_string().to_lowercase().ends_with(".gz") {
-                                            // Return a Gzip reader
-                                            Box::new(GzDecoder::new(File::open(p)?))
-                                        } else {
-                                            // Return plain file reader
-                                            Box::new(File::open(p)?)
-                                        };
-
-        let mut reader = csv::ReaderBuilder::new()
-                                .delimiter(delimiter)
-                                .from_reader(file_reader);
-
-        // TODO: Don't fail on non existant headers -> give 'col0', 'col1', etc.
-        let headers: Vec<String> = reader.headers()?
-                                        .clone()
-                                        .into_iter()
-                                        .map(|v| v.to_string())
-                                        .collect();
-
-        // Containers for storing column data
-        let mut vecs: Vec<Vec<String>> = (0..headers.len())
-                                            .map(|_| Vec::new())
-                                            .collect();
-
-        for record in reader.records() {
-
-            match record {
-
-                Ok(rec) => {
-                    for (field, container) in rec.iter().zip(&mut vecs) {
-                        container.push(field.into());
-                    };
-                },
-
-                // TODO: Process for dealing with invalid records.
-                Err(err) => println!("Unable to read record: '{}'", err)
-            }
-        }
-
-        let mut df = DataFrame::new();
-
-        // map headers to vectors containing it's fields in parallel and into
-        // Series structs, parsing each field.
-        // TODO: Parallelize this operation, parse && serialize columns in parallel, then add them.
-        let _ = headers
-            .into_iter()
-            .zip(vecs)
-            .map(|(header, vec)| {
-                let mut series = Series::from_vec(vec);
-                series.set_name(&header);
-                if let Ok(ser) = series.astype::<i32>() {
-                    df.add_column(ser).unwrap();
-                } else if let Ok(ser) = series.astype::<f32>() {
-                    df.add_column(ser).unwrap()
-                } else {
-                    df.add_column(series).unwrap()
-                }
-            })
-            .collect::<Vec<()>>();
-        Ok(df)
-    }
-
-
-    /// Write a dataframe to CSV, consumes self, and thus will not double memory whilst
-    /// writing to CSV.
-    ///
-    /// ## Example
-    /// ```
-    /// use blackjack::prelude::*;
-    ///
-    /// let mut df = DataFrame::new();
-    ///
-    /// df.add_column(Series::arange(0, 10));
-    /// df.add_column(Series::arange(0, 10));
-    ///
-    /// let result = df.into_csv("/tmp/test.csv.gz", b',').is_ok(); // Gzip compression inferred.
-    /// assert_eq!(result, true);
-    /// ```
-    pub fn into_csv<S>(self, path: S, delimiter: u8) -> Result<(), BlackJackError>
-        where S: AsRef<OsStr> + ToString
-    {
-        use std::io::prelude::*;
-        use std::fs::File;
-        use flate2::read::GzEncoder;
-        use flate2::Compression;
-
-        let p = Path::new(&path);
-
-        let file_writer: Box<Write> = if path.to_string().to_lowercase().ends_with(".gz") {
-                                            // Return a Gzip reader
-                                            Box::new(GzEncoder::new(File::create(p)?, Compression::default()))
-                                        } else {
-                                            // Return plain file reader
-                                            Box::new(File::create(p)?)
-                                        };
-
-        let mut writer = csv::WriterBuilder::new()
-            .delimiter(delimiter)
-            .from_writer(file_writer);
-
-        let header = self.columns().map(|v| v.to_string()).collect::<Vec<String>>();
-
-        // Deserialize all series into string vecs
-        let mut data = vec![];
-        for serialized_series in self.data {
-            let series_container = serialized_series.decode_infer()?;
-            let string_vec = series_container.into_string_vec();
-            data.push(string_vec);
-        }
-
-        // Write out records
-        writer.write_record(header.as_slice())?;
-
-        // TODO: Probably a better way to do this?
-        for row_idx in 0..data[0].len() {
-            let mut row = vec![];
-            for column_idx in 0..data.len() {
-                row.push(&data[column_idx][row_idx]);
-            }
-            writer.write_record(row.as_slice())?;
-        }
-
-        Ok(())
     }
 }
