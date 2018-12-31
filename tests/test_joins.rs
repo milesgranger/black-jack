995fe164d09ff92d11029c350981cd7fc8f2848b
Miles Granger <miles59923@gmail.com>
2018-12-31 20:13:20 +0100

    Support Series::drop_positions

--- tests/test_joins.rs
+++ tests/test_joins.rs
@@ -2,6 +2,6 @@ use blackjack::prelude::*;
 
 #[test]
 fn test_basic_join() {
-    let _series = Series::from_vec(vec![0, 1, 2, 3, 4]);
-
+    let series1 = Series::from_vec(vec![0, 1, 2, 3, 4]);
+    let series2 = Series::from_vec(vec![1, 2, 3, 4, 5]);
 }
