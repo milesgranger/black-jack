b618074b72b5122d9875dd0cad3e82880d41e570
Miles Granger <miles59923@gmail.com>
2019-01-01 05:37:19 +0100

    Support Series::drop_indexes

--- tests/test_series_mods.rs
+++ tests/test_series_mods.rs
@@ -21,3 +21,35 @@ fn test_drop_positions() {
     assert_eq!(series.len(), 4);
     assert_eq!(series.values, vec![1, 2, 3, 5]);
 }
+
+#[test]
+fn test_drop_indexes() {
+
+    let mut series = Series::from_vec(vec![0, 1, 2, 3, 4, 5]);
+
+    // before drops
+    assert_eq!(series.len(), 6);
+    assert_eq!(series.values, vec![0, 1, 2, 3, 4, 5]);
+
+    // Drop and check results
+    series.drop_indexes(vec![0, 4]);
+    assert_eq!(series.len(), 4);
+    assert_eq!(series.values, vec![1, 2, 3, 5]);
+
+
+
+    // Again but changing the index values
+    let mut series = Series::from_vec(vec![0, 1, 2, 3, 4, 5]);
+
+    // change the index values should have an effect.
+    assert!(series.set_index(vec![1, 2, 3, 4, 5, 6]).is_ok());
+
+    // before drops
+    assert_eq!(series.len(), 6);
+    assert_eq!(series.values, vec![0, 1, 2, 3, 4, 5]);
+
+    // Drop and check results
+    series.drop_indexes(vec![0, 4]);  // 0 isn't any index vale, 4 matches 3
+    assert_eq!(series.len(), 5);
+    assert_eq!(series.values, vec![0, 1, 2, 4, 5]);
+}
